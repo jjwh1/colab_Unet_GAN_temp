724988614ffc0d9d835a1170cdce901abf502a8b
jjwh1 <wonho3327@naver.com>
2024-12-24 23:12:09 +0900

    hole_loss추가

--- train.py
+++ train.py
@@ -61,7 +61,7 @@ def train_gan_epoch(generator, discriminator, dataloader, criterion, optimizer_g
         optimizer_g.zero_grad()
         fake_output = discriminator(fake_images)
         g_loss_adv = criterion(fake_output, torch.ones_like(fake_output).to(device))  # Discriminator를 잘 속이는지에 대한 지표(loss)
-        g_loss_pixel = nn.MSELoss()(fake_images, gts)  # Discriminator와 관계없이 gt image와 비교했을 때 잘 복원했는지에 대한 지표(loss)
+        g_loss_pixel = nn.MSELoss()(fake_images*(1-masks), gts*(1-masks)) + 6*nn.L1Loss()(fake_images*masks, gts*masks)  # Discriminator와 관계없이 gt image와 비교했을 때 잘 복원했는지에 대한 지표(loss)
         g_loss = g_loss_pixel + lambda_adv * g_loss_adv
         g_loss.backward()
         optimizer_g.step()
@@ -191,18 +191,19 @@ def load_checkpoint(checkpoint_path, generator, discriminator, optimizer_g, opti
 
 def main():
     # Paths
-    save_dir = "D:/inpaint_result/CASIA_Lamp/Unet_GAN_temp/db1_train"
+    
+    save_dir = "/content/drive/MyDrive/inpaint_result/CASIA_Lamp/Unet_GAN_temp_add_holeloss/db1_train"
     writer = SummaryWriter(os.path.join(save_dir, 'SR_Stage_4%s' % datetime.now().strftime("%Y%m%d-%H%M%S")))
 
-    train_image_paths = 'C:/Users/8138/Desktop/DB/CASIAv4/CASIA_Ver4_Distance/reflection_random(50to1.7)_db2_224_trainset'  # List of input image paths
-    train_mask_paths = 'D:/mask/CASIA_Distance/algorithm/450to50000_174x174padding_if_gac1_4000_algorithm/db2_test_layer28_0.3_only_mask_trainset'  # List of mask paths
-    train_gt_paths = "C:/Users/8138/Desktop/DB/CASIAv4/CASIA_Ver4_Distance/db2_224_for_gt_inpainting_trainset"  # List of ground truth paths
-    train_large_mask_paths = "D:/mask/CASIA_Distance/algorithm/450to50000_174x174padding_if_gac1_4000_algorithm/db2_test_layer28_0.3_only_mask_large_mask_h2_w2.2_trainset"  # List of ground truth paths
+    train_image_paths = '/content/dataset/CASIA_Ver4_Distance/reflection_random(50to1.7)_db1_224_trainset'  # List of input image paths
+    train_mask_paths = '/content/dataset/CASIA_Distance/algorithm/450to50000_174x174padding_if_gac1_4000_algorithm/db1_test_layer28_0.3_only_mask_trainset'  # List of mask paths
+    train_gt_paths = "/content/dataset/CASIA_Ver4_Distance/db1_224_for_gt_inpainting_trainset"  # List of ground truth paths
+    train_large_mask_paths = "/content/dataset/CASIA_Distance/algorithm/450to50000_174x174padding_if_gac1_4000_algorithm/db1_test_layer28_0.3_only_mask_large_mask_h2_w2.2_trainset"  # List of ground truth paths
 
-    val_image_paths = 'C:/Users/8138/Desktop/DB/CASIAv4/CASIA_Ver4_Distance/reflection_random(50to1.7)_db2_224_validset'  # List of input image paths
-    val_mask_paths = 'D:/mask/CASIA_Distance/algorithm/450to50000_174x174padding_if_gac1_4000_algorithm/db2_test_layer28_0.3_only_mask_validset'  # List of mask paths
-    val_gt_paths = "C:/Users/8138/Desktop/DB/CASIAv4/CASIA_Ver4_Distance/db2_224_for_gt_inpainting_validset"  # List of ground truth paths
-    val_large_mask_paths = "D:/mask/CASIA_Distance/algorithm/450to50000_174x174padding_if_gac1_4000_algorithm/db2_test_layer28_0.3_only_mask_large_mask_h2_w2.2_validset"  # List of ground truth paths
+    val_image_paths = '/content/dataset/CASIA_Ver4_Distance/reflection_random(50to1.7)_db1_224_validset'  # List of input image paths
+    val_mask_paths = '/content/dataset/CASIA_Distance/algorithm/450to50000_174x174padding_if_gac1_4000_algorithm/db1_test_layer28_0.3_only_mask_validset'  # List of mask paths
+    val_gt_paths = "/content/dataset/CASIA_Ver4_Distance/db1_224_for_gt_inpainting_validset"  # List of ground truth paths
+    val_large_mask_paths = "/content/dataset/CASIA_Distance/algorithm/450to50000_174x174padding_if_gac1_4000_algorithm/db1_test_layer28_0.3_only_mask_large_mask_h2_w2.2_validset"  # List of ground truth paths
 
     results_path = os.path.join(save_dir, "metrics.csv")
 
@@ -212,7 +213,7 @@ def main():
     checkpoint_path = None
 
     # Parameters
-    batch_size = 8
+    batch_size = 16
     lr = 0.0002
     num_epochs = 300
     lambda_adv = 0.1

b9e75c9d7e11da93943dbfd99aad707c444d545f
jjwh1 <wonho3327@naver.com>
2025-01-21 15:59:26 +0900

    Unet_GAN_add_L2_hole_loss_100_recogL2lossfold1_0.1_colab_

--- train_recogloss.py
+++ train_recogloss.py
@@ -231,12 +231,12 @@ def main():
     train_image_paths = '/content/dataset/reflection_random(50to1.7)_db1_224_trainset'  # List of input image paths
     train_mask_paths = '/content/dataset/CASIA_Lamp/algorithm/450to50000_174x174padding_if_gac1_4000_algorithm/db1_test_layer28_0.3_only_mask_trainset'  # List of mask paths
     train_gt_paths = "/content/dataset/db1_224_for_gt_inpainting_trainset"  # List of ground truth paths
-    train_large_mask_paths = "/content/dataset/CASIA_Lamp/algorithm/450to50000_174x174padding_if_gac1_4000_algorithm/db1_test_layer28_0.3_only_mask_large_mask_h2.6_w2.8_trainset"  # List of ground truth paths
+    train_large_mask_paths = "/content/dataset/CASIA_Lamp/algorithm/450to50000_174x174padding_if_gac1_4000_algorithm/db1_test_layer28_0.3_only_mask_large_mask_h2.8_w3_trainset"  # List of ground truth paths
 
     val_image_paths = '/content/dataset/reflection_random(50to1.7)_db1_224_validset'  # List of input image paths
     val_mask_paths = '/content/dataset/CASIA_Lamp/algorithm/450to50000_174x174padding_if_gac1_4000_algorithm/db1_test_layer28_0.3_only_mask_validset'  # List of mask paths
     val_gt_paths = "/content/dataset/db1_224_for_gt_inpainting_validset"  # List of ground truth paths
-    val_large_mask_paths = "/content/dataset/CASIA_Lamp/algorithm/450to50000_174x174padding_if_gac1_4000_algorithm/db1_test_layer28_0.3_only_mask_large_mask_h2.6_w2.8_validset"  # List of ground truth paths
+    val_large_mask_paths = "/content/dataset/CASIA_Lamp/algorithm/450to50000_174x174padding_if_gac1_4000_algorithm/db1_test_layer28_0.3_only_mask_large_mask_h2.8_w3_validset"  # List of ground truth paths
 
     results_path = os.path.join(save_dir, "metrics.csv")
 

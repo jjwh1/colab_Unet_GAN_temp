4e98699837bb4d961d01ad92b9e22d0ea8e68f4a
jjwh1 <wonho3327@naver.com>
2025-04-28 15:51:32 +0900

    sdf

--- DeepFill_v2/train_U.py
+++ DeepFill_v2/train_U.py
@@ -262,8 +262,8 @@ def main():
 
     os.makedirs(save_dir, exist_ok=True)
 
-    # checkpoint_path = r"D:\inpaint_result\CASIA_Distance\HiFill_fold1_py\db1_train/checkpoint_epoch_150.tar"  # 불러올 시 마지막 저장된 pth파일 경로 입력!!
-    checkpoint_path = None
+    checkpoint_path = "/content/drive/MyDrive/inpaint_result/UPOL/DeepFillv2_lr_00010001_fold1_colab_re/db1_train/checkpoint_epoch_300.tar"  # 불러올 시 마지막 저장된 pth파일 경로 입력!!
+    # checkpoint_path = None
 
     # Parameters
     batch_size = 4

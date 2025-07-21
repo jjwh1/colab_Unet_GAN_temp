82d22994e1c585920d2e983436ad9af0fcbe6737
jjwh1 <wonho3327@naver.com>
2025-07-21 21:51:32 +0900

    sdf

--- KD_compare/hyper_ablation_train_lamp_fold2.py
+++ KD_compare/hyper_ablation_train_lamp_fold2.py
@@ -24,8 +24,8 @@ def seed_everything(seed):
     torch.manual_seed(seed) #torch를 거치는 모든 난수들의 생성순서를 고정한다
     torch.cuda.manual_seed(seed) #cuda를 사용하는 메소드들의 난수시드는 따로 고정해줘야한다
     torch.cuda.manual_seed_all(seed)  # if use multi-GPU
-    torch.backends.cudnn.deterministic = True #딥러닝에 특화된 CuDNN의 난수시드도 고정
-    torch.backends.cudnn.benchmark = False
+    # torch.backends.cudnn.deterministic = True #딥러닝에 특화된 CuDNN의 난수시드도 고정
+    # torch.backends.cudnn.benchmark = False
     np.random.seed(seed) #numpy를 사용할 경우 고정
     random.seed(seed) #파이썬 자체 모듈 random 모듈의 시드 고정
 seed_everything(42)
@@ -423,7 +423,7 @@ def main():
                 psnr, ssim
             ])
 
-        if epoch >= 250:
+        if epoch >= 270:
             # Save checkpoint
             torch.save({
                 "epoch": epoch + 1,
